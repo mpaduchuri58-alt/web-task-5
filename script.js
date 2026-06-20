b67d02da44705c9992dfe47af52aa74398bffecc
pujitha <mpaduchuri58@jnn.edu.in>
2026-06-20 12:10:23 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+async function getWeather() {
+
+  
+  const city =
+    document.getElementById("city").value;
+
+  
+  const apiKey = "YOUR_API_KEY";
+
+  
+  const url =
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+  try {
+
+  
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    
+    if (data.cod === 200) {
+
+      document.getElementById("weatherResult")
+        .innerHTML = `
+
+        <h2>${data.name}</h2>
+
+        <p>🌡 Temperature:
+        ${data.main.temp} °C</p>
+
+        <p>☁ Weather:
+        ${data.weather[0].description}</p>
+
+        <p>💧 Humidity:
+        ${data.main.humidity}%</p>
+
+        <p>🌬 Wind Speed:
+        ${data.wind.speed} m/s</p>
+      `;
+
+    } else {
+
+      document.getElementById("weatherResult")
+        .innerHTML = "City not found!";
+    }
+
+  } catch (error) {
+
+    document.getElementById("weatherResult")
+      .innerHTML = "Error fetching weather data!";
+  }
+}
